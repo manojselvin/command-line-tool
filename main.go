9a44a1a2b83b9c918be41aa733168fa1a74becd8
Manoj Selvin <mselvin@nuskin.com>
2024-04-08 12:47:41 +0530

    New: Init repo with main.go

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+// Todo represents a TODO item
+type Todo struct {
+	UserID    int    `json:"userId"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+func main() {
+
+}
